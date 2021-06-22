1806d6686b0dc23865a78ad8dc691a1532782dfc
Priyam Paul <priyam.paul1@gmail.com>
2021-06-22 23:19:32 +0530

    commiting after defining function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script to inverse the matrix entered
 
-## Write a short comment describing this function
+## Make Cache Matrix to get and set the methods
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+  inv <- NULL
+  set <- function(y){
+    x<<-y
+    inv <<-NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+## cacheSolve the calling function 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("PRIYAM: getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
